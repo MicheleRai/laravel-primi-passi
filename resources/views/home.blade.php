c6df9446fb0752b69fdb17c30cad1447f0d7ad30
MicheleRai <rainonemichele1999@gmail.com>
2023-01-12 13:27:47 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,33 @@
+<?php
+$myArray = ['Hello', 'World'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        h1{
+            text-align: center
+        }
+        .red{
+            color: red;
+        }
+        .blue{
+            color: blue;
+        }
+    </style>
+</head>
+<body>
+    <h1>
+        @foreach ($myArray as $parola)
+
+            <span class="{{$loop->first ? 'red' : 'blue'}}">{{$parola}}</span>
+
+        @endforeach
+    </h1>
+</body>
+</html>
